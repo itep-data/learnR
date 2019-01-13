ea8cb254f4ee47315f14107d91a79162a32a11e3
Kvale <Dorian.Kvale@state.mn.us>
2019-01-12 22:54:52 -0600

    update tabsets

--- content/page/insert_tabs.R
+++ content/page/insert_tabs.R
@@ -3,40 +3,51 @@
 
 add_tabs <- function(tab_names, tab_content) {
 
-  text_out <- c()
+  text_out <- c('<ul class="nav nav-pills" id="myTab" role="tablist" style="margin-top: 18px;">\n')
+
+  tab_codes <- tolower(gsub(" ", "", tab_names))
 
   for(i in 1:length(tab_names)) {
 
-    new_text <- paste0(
-          '<ul class="nav nav-pills" id="myTab" role="tablist" style="margin-top: 18px;">',
-             ifelse(i == 1, '<li class="nav-item active">', '<li class="nav-item">'),
-                '<a class="nav-link" id="', tab_names[[i]],
-                '-tab" data-toggle="tab" href="#', tab_names[[i]],
-                '" role="tab" aria-controls="', tab_names[[i]],
+    new_text <- paste0(ifelse(i == 1, '<li class="nav-item active">', '<li class="nav-item">'),
+                '<a class="nav-link" id="', tab_codes[[i]],
+                '-tab" data-toggle="tab" href="#', tab_codes[[i]],
+                '" role="tab" aria-controls="', tab_codes[[i]],
                 ifelse(i == 1, '" aria-selected="true">', '" aria-selected="false">'),
           tab_names[[i]],
-                '</a></li>')
+                '</a></li>\n')
 
     text_out <- paste0(text_out, new_text)
   }
 
   text_out <- paste0(text_out,
-                     '</ul>
-                     <div class="well tab-content" id="myTabContent" style="background-color: white;">')
+'</ul>\n<div class="well tab-content" id="myTabContent" style="background-color: white;">\n\n')
 
   for(i in 1:length(tab_names)) {
 
-    new_text <- paste0(ifelse(i == 1, '<div class="tab-pane fade active in" id="', '<div class="tab-pane fade" id="'),
-                       tab_names[[i]],
-                       '" role="tabpanel" aria-labelledby="', tab_names[[i]], '-tab">',
-                       tab_content[[i]], '</div>'
+    new_text <- paste0(ifelse(i == 1, '\n<div class="tab-pane fade active in" id="', '<div class="tab-pane fade" id="'),
+                       tab_codes[[i]],
+                       '" role="tabpanel" aria-labelledby="', tab_codes[[i]], '-tab">\n\n',
+                       tab_content[[i]], '\n\n</div>\n'
                        )
 
     text_out <- paste0(text_out, new_text)
   }
 
 
-  text_out <- paste0(text_out, '</div>')
+  text_out <- paste0(text_out, '\n</div>')
+
+  text_out <- readLines(textConnection(text_out))
+
+  # Sub in <code> for backticks ```
+  code_starts <- grep("code_start", text_out)
+
+  text_out[code_starts] <- '<pre class="sourceCode r"><code class="sourceCode r">'
+
+  code_ends <- grep("code_end", text_out)
+
+  text_out[code_ends] <- '</code></pre>'
+
 
   return(text_out)
 
